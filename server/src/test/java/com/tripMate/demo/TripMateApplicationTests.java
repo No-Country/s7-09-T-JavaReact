842aeaae91ad4b1eaafc0613797d2b37a62527d1
gabusy07 <jin_thecraziest@live.com>
2023-03-25 14:48:31 -0300

    fix: raname folders

--- server/src/test/java/com/tripMate/demo/TripMateApplicationTests.java
+++ server/src/test/java/com/tripMate/demo/TripMateApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tripMate.noCountry;
+package com.tripMate.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class TripMasticationTests {
 
 	@Test
 	void contextLoads() {
