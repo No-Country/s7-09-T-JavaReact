0be41feca72832b02dda37de947e71ad1f01e04a
adrianmcenturion <adrianmcenturion@gmail.com>
2023-03-27 19:33:15 -0300

    inital commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1 className='font-bold text-2xl text-center text-red-400'>TRIPMATE</h1>
+    </div>
+  )
+}
+
+export default App
