dcaf10843c393a293ccf27170bbdaf47f929e81d
gabusy07 <jin_thecraziest@live.com>
2023-03-27 19:07:56 -0300

    create: init branch backend

--- server/src/main/java/com/tripMate/demo/config/SwaggerConfig.java
+++ server/src/main/java/com/tripMate/demo/config/SwaggerConfig.java
@@ -1,18 +1,15 @@
 package com.tripMate.demo.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
 
+    /*
     private final String controllersBasePackage = "com.tripMate.demo.controller";
 
     @Bean
@@ -31,6 +28,6 @@ public class SwaggerConfig {
                 "Terms of service",
                 new Contact("", "", ""),
                 "", "", Collections.emptyList());
-    }
+    }*/
 
 }
