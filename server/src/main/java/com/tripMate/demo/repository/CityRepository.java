cc20b3656590faec050c037173f0f75cf83fdc1f
Analía <100878852+AnaliaCasali@users.noreply.github.com>
2023-03-28 15:38:05 -0300

    Update CityRepository.java

--- server/src/main/java/com/tripMate/demo/repository/CityRepository.java
+++ server/src/main/java/com/tripMate/demo/repository/CityRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 
-    @Query(value = "SELECT * FROM cities  WHERE city LIKE '%:search%' OR  province LIKE '%:search%' OR  country LIKE '%:search%'   ", nativeQuery = true)
+    @Query(value = "SELECT * FROM cities  WHERE city LIKE %:search% OR  province LIKE %:search% OR  country LIKE %:search%   ", nativeQuery = true)
     List<City> findAllByCityOrProvinceOrCountryContaining(@Param("search") String text);
 
 }
