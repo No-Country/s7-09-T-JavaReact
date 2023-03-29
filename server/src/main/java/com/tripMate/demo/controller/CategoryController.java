c1b00adca97f0f93d3ad082acaf0e49112fb24dd
Lihuel <lihuelmujica@gmail.com>
2023-03-29 11:15:55 -0300

    add: Integrate Swagger for REST API documentation

--- server/src/main/java/com/tripMate/demo/controller/CategoryController.java
+++ server/src/main/java/com/tripMate/demo/controller/CategoryController.java
@@ -1,13 +1,17 @@
 package com.tripMate.demo.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/category")
 public class CategoryController {
     @GetMapping("/hola")
-    public String ggg(){
-        return "hello";
+    public ResponseEntity<String> ggg(){
+        return new ResponseEntity<>("hola", HttpStatus.OK);
     }
 }
