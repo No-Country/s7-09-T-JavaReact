b0b8de7d53094575391c21655fc0eb8f817565bc
Analía <100878852+AnaliaCasali@users.noreply.github.com>
2023-03-31 16:34:47 -0300

    Update CityDTO.java

--- server/src/main/java/com/tripMate/demo/dto/CityDTO.java
+++ server/src/main/java/com/tripMate/demo/dto/CityDTO.java
@@ -1,13 +1,22 @@
 package com.tripMate.demo.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 @Data
 public class CityDTO {
-    private Long id;
-
+    private int id;
     private String city;
-
     private String province;
     private String country;
 
