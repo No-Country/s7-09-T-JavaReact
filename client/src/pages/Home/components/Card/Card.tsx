df50ffbdfd29b7e7efa0e39e4711b97ae6f75399
adrianmcenturion <adrianmcenturion@gmail.com>
2023-04-02 01:41:31 -0300

    fix error in card component

--- client/src/pages/Home/components/Card/Card.tsx
+++ client/src/pages/Home/components/Card/Card.tsx
@@ -1,13 +1,13 @@
 type Props = {
     name: string
-    icon?: string | null
+    icon?: string
     param?: string
 }
 
 const LinkCard = ({name, icon, param}: Props) => {
   return (
     <div className="flex flex-1 flex-col items-center justify-center rounded-xl p-3 border-[1px] border-black border-opacity-25 border-solid hover:cursor-pointer">
-        <span>{icon ? <img className="" src={icon} alt={name} /> : 'ICO'}</span>
+        <span><img className="" src={icon} alt={name} /></span>
         <p className="font-medium text-sm text-black">{name}</p>
     </div>
   )
