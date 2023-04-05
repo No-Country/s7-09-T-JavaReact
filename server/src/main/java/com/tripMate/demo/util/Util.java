f52ee8dd7530eb597c86cd3e5fd862b9725e1c5d
LihuelMujica <lihuelmujica@gmail.com>
2023-04-05 14:29:59 -0300

    add: getNullPropertyNames and mergeObjects methods to Util

--- server/src/main/java/com/tripMate/demo/util/Util.java
+++ server/src/main/java/com/tripMate/demo/util/Util.java
@@ -1,4 +1,26 @@
 package com.tripMate.demo.util;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Util {
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper wrappedSource = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = wrappedSource.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = wrappedSource.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+    public static void mergeObjects(Object src, Object target) {
+        BeanUtils.copyProperties(src, target, getNullPropertyNames(src));
+    }
 }
