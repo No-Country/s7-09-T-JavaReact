b0bc25e6d51b8c1183ce8b6689a807e4cfac5361
LihuelMujica <lihuelmujica@gmail.com>
2023-04-05 14:45:30 -0300

    add: getUserByEmail to UserService interface

--- server/src/main/java/com/tripMate/demo/service/UserService.java
+++ server/src/main/java/com/tripMate/demo/service/UserService.java
@@ -2,17 +2,18 @@ package com.tripMate.demo.service;
 
 import com.tripMate.demo.dto.UserCreateDTO;
 import com.tripMate.demo.dto.UserDTO;
+import com.tripMate.demo.exception.ResourceAlreadyExistsException;
 import com.tripMate.demo.exception.ResourceNotFoundException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-import java.util.List;
 
 public interface UserService extends UserDetailsService {
 
-    UserDTO createUser (UserCreateDTO userCreateDTO, String role);
+    UserDTO createUser (UserCreateDTO userCreateDTO, String role) throws ResourceNotFoundException, ResourceAlreadyExistsException;
 
     UserDTO getUserById(int id) throws ResourceNotFoundException;
+    UserDTO getUserByEmail(String email) throws ResourceNotFoundException;
 
-    UserDTO updateUser(int id, UserDTO userDTO);
-    void deleteUser(int id);
+    UserDTO updateUser(int id, UserCreateDTO userDTO) throws ResourceNotFoundException;
+    void deleteUser(int id) throws ResourceNotFoundException;
 }
