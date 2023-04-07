2a32706da8cba0dd469c14e59256eecf585a2043
adrianmcenturion <adrianmcenturion@gmail.com>
2023-04-07 01:03:35 -0300

    feature contact view with proper styles

---
+++ client/src/pages/Contact/ContactData.ts
@@ -0,0 +1,47 @@
+export const contacts = [
+    {
+      name: "Adrián Centurion",
+      role: "Front-end",
+      img: "",
+    },
+    {
+      name: "Darío Elguero",
+      role: "Front-end",
+      img: "",
+    },
+    {
+      name: "Jordi Mantilla",
+      role: "Front-end",
+      img: "",
+    },
+    {
+      name: "Lihuel Mujica",
+      role: "Back-end",
+      img: "",
+    },
+    {
+      name: "Christian Herrera",
+      role: "Back-end",
+      img: "",
+    },
+    {
+      name: "Gerardo Vargas",
+      role: "Back-end",
+      img: "",
+    },
+    {
+      name: "Analía Casali",
+      role: "Back-end",
+      img: "",
+    },
+    {
+      name: "Gabriel Mayantz",
+      role: "Diseño UX/UI",
+      img: "",
+    },
+    {
+      name: "Andrés Cajas",
+      role: "Diseño UX/UI",
+      img: "",
+    },
+  ];
