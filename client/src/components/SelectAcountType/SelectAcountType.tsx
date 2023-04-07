3407d65a2b67d3c791732dc79a8b4944b2942313
Dario Elguero <darioelguero@gmail.com>
2023-04-07 18:50:12 -0300

    added view desktop in register

--- client/src/components/SelectAcountType/SelectAcountType.tsx
+++ client/src/components/SelectAcountType/SelectAcountType.tsx
@@ -10,27 +10,29 @@ interface Props {
 const SelectAcountType = ({ setType }: Props) => {
   return (
     <div className="flex flex-col items-center w-full">
-      <img className="w-[18.5rem] my-12" src={logo} alt="tripmate" />
-      <div className="flex flex-col p-4 w-full">
-        <p className="font-bold">Selecciona el tipo de cuenta</p>
-        <input
-          type="submit"
-          value={"Cuenta personal"}
-          onClick={() => setType(AcountTypeEnum.PERSONAL)}
-          className="rounded-md bg-[#FF5C00] text-white font-medium w-full h-10 mt-10"
-        />
-        <input
-          type="submit"
-          value={"Soy proveedor"}
-          onClick={() => setType(AcountTypeEnum.SUPPLIER)}
-          className="rounded-md bg-white text-[#FF5C00] border-[#FF5C00] border-solid border-[0.0625rem] font-medium w-full h-10 mt-4"
-        />
-      </div>
-      <div className="flex gap-2 mt-2">
-        <span>Volver a </span>
-        <Link to={`/${PublicRoutes.LOGIN}`}>
-          <span className="text-[#FF5C00] font-bold">Inicio de sesión</span>
-        </Link>
+      <div className="flex flex-col w-full items-center h-[30rem] sm:w-[25.5rem] sm:px-6 sm:my-10 sm:border sm:moder-[1px] sm:rounded-lg">
+        <img className="w-[18.5rem] my-12" src={logo} alt="tripmate" />
+        <div className="flex flex-col p-4 w-full">
+          <p className="font-bold">Selecciona el tipo de cuenta</p>
+          <input
+            type="submit"
+            value={"Cuenta personal"}
+            onClick={() => setType(AcountTypeEnum.PERSONAL)}
+            className="rounded-md bg-[#FF5C00] text-white font-medium w-full h-10 mt-10"
+          />
+          <input
+            type="submit"
+            value={"Soy proveedor"}
+            onClick={() => setType(AcountTypeEnum.SUPPLIER)}
+            className="rounded-md bg-white text-[#FF5C00] border-[#FF5C00] border-solid border-[0.0625rem] font-medium w-full h-10 mt-4"
+          />
+        </div>
+        <div className="flex gap-2 mt-2">
+          <span>Volver a </span>
+          <Link to={`/${PublicRoutes.LOGIN}`}>
+            <span className="text-[#FF5C00] font-bold">Inicio de sesión</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
