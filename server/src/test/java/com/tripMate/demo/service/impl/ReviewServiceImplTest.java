f175792da4b377975962dc513b06ba681420ad42
LihuelMujica <lihuelmujica@gmail.com>
2023-04-15 13:37:54 -0300

    test: add shouldThrowExceptionWhenScoreIsHigherThan5InUpdateReview

--- server/src/test/java/com/tripMate/demo/service/impl/ReviewServiceImplTest.java
+++ server/src/test/java/com/tripMate/demo/service/impl/ReviewServiceImplTest.java
@@ -270,6 +270,23 @@ class ReviewServiceImplTest {
     }
 
     @Test
+    void shouldThrowExceptionWhenScoreIsHigherThan5InUpdateReview() {
+        //given
+        Experience experience = experienceList.get(0);
+        User user = userList.get(0);
+        ReviewCreateDTO reviewCreate = new ReviewCreateDTO(6, "Muy bueno");
+        when(reviewRepository.save(any(Review.class))).thenAnswer(invocation -> {
+            Review review = invocation.getArgument(0);
+            review.setId(5);
+            return review;
+        });
+        when(experienceRepository.findById(experience.getId())).thenReturn(Optional.of(experience));
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        //when and then
+        assertThrows(Exception.class, () -> reviewService.createReview(reviewCreate, experience.getId(), user.getEmail()));
+    }
+
+    @Test
     void shouldThrowExceptionWhenScoreIsLowerThan0InUpdateReview() {
         //given
         Experience experience = experienceList.get(0);
