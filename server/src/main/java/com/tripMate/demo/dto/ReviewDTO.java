e22817a176cbef703e5eea06c8ba49b863ebd020
LihuelMujica <lihuelmujica@gmail.com>
2023-04-16 07:16:46 -0300

    add: date to review

--- server/src/main/java/com/tripMate/demo/dto/ReviewDTO.java
+++ server/src/main/java/com/tripMate/demo/dto/ReviewDTO.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -13,6 +15,7 @@ public class ReviewDTO {
     private int id;
     private int score;
     private String review;
+    private LocalDate date;
     private Profile profile;
     private int experienceId;
 }
